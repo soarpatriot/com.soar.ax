22d38552535e3b7ab4684943be427e7fc240a219
soar <85624529@qq.com>
2012-03-05 22:58:56 +0800

    add

---
+++ ax-web/src/main/java/com/soar/ax/view/XlsView.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.soar.ax.view;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
+import org.springframework.web.servlet.view.document.AbstractJExcelView;
+
+/**
+ * @author liuhb7
+ *
+ */
+public class XlsView extends AbstractJExcelView {
+	protected void buildExcelDocument (Map model,
+			WritableWorkbook wb,
+			HttpServletRequest request,
+			HttpServletResponse response)
+			throws Exception {
+			WritableSheet sheet = wb.createSheet("Spring", 0);
+			sheet.addCell(new Label(0, 0, "Spring-Excel test"));
+			List words = (List) model.get("wordList");
+			/*for (int i = 0; i < words.size(); i++) {
+			  sheet.addCell(new Label(2+i, 0, (String) words.get(i)));
+			}*/
+		    response.setHeader("Content-Disposition", "attachment; filename=\"file.xls\""); 
+
+	}
+}
